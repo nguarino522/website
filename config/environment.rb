60a3e2b78c773a50243e54976fb24e0ddf0af299
Nicholas Guarino <sub4miler5@gmail.com>
2022-07-09 16:52:39 -0400

    attempt to fix deploy to heroku

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,6 @@
 require 'bundler'
 Bundler.require
-ActiveRecord::Base.establish_connection(
-:adapter => 'sqlite3',
-:database => 'db/development.sqlite'
-)
+
+set :database_file, “./database.yml”
+
 require_all 'app'
