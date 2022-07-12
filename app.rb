5ea2bc809d5c35e0aa5753212f26702f9571a5b1
Nicholas Guarino <sub4miler5@gmail.com>
2022-07-11 21:44:09 -0400

    added routes and page for each

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
-  require 'sinatra'
-  # require "erb"
+require 'sinatra'
+ # require "erb"
 
   # configure { set :server, :puma }
 
@@ -7,32 +7,40 @@
   # set :bind, '192.168.1.9'
 
 
-  get '/' do
-    erb :index
-  end
+get '/' do
+  erb :index
+end
+
+get '/about' do
+  erb :about
+end
+
+get '/skills' do
+  erb :skills
+end
+
+get '/projects' do
+  erb :projects
+end
+
+get '/contact' do
+  erb :contact
+end
+
 
 
   # get '/:background' do
   #   erb :base, { :locals => params, :layout => false }
   # end
+get '/time' do
+  code = "<%= Time.now %>"
+  erb code
+end
 
-  get '/time' do
-    code = "<%= Time.now %>"
-    erb code
-  end
+get '/test' do
+  erb :test
+end
 
-  get '/test' do
-    erb :test
-  end
-
-  get '/sinatra' do
-    erb :sinatra
-  end
-
-  # get '/footer' do
-  #   erb :footer, :layout => :sinatra
-  # end
-
-  get '/background' do
-    erb :background
-  end
+get '/sinatra' do
+  erb :sinatra
+end
